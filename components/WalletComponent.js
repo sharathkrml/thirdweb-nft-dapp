8e5f3e93691fbeaca678a2850c2a3bb1b328c824
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2022-04-04 11:32:34 +0530

    modified modal

--- components/WalletComponent.js
+++ components/WalletComponent.js
@@ -9,33 +9,34 @@ function WalletComponent() {
   const connectMetamask = useMetamask();
   const connectCoinbaseWallet = useCoinbaseWallet();
   const connectWalletConnect = useWalletConnect();
+
   return (
     <div className="flex justify-around items-center h-[100%] lg:flex-row flex-col sm:flex-row">
-      <div className="metamask">
-        <button
+      <div className="metamask flex flex-col items-center w-[80%] h-[25%]">
+        <div
           onClick={connectMetamask}
-          className="flex p-10  items-center justify-center bg-white rounded-lg hover:scale-105"
+          className="flex self-center  items-center w-[80%] h-[90%] justify-center bg-white rounded-lg hover:scale-105"
         >
-          <Image src="/wallet/metamask.svg" height={80} width={80}></Image>
-        </button>
+          <img src="/wallet/metamask.svg" className="self-center w-[50%] " />
+        </div>
         <p className="text-white text-center mt-2">Connect Metamask</p>
       </div>
-      <div className="walletConnect">
-        <button
+      <div className="walletConnect w-[80%] h-[25%] flex flex-col items-center w-[80%] h-[25%]">
+        <div
           onClick={connectWalletConnect}
-          className="flex p-10 items-center justify-center bg-white rounded-lg hover:scale-105"
+          className="flex items-center border-2 border-white w-[80%] h-[80%] justify-center bg-white rounded-lg hover:scale-105"
         >
-          <Image src="/wallet/walletConnect.svg" height={80} width={80}></Image>
-        </button>
+          <img src="/wallet/walletConnect.svg" className="self-center w-[50%]" />
+        </div>
         <p className="text-white text-center mt-2">Connect Wallet Connect</p>
       </div>
-      <div className="coinbase">
-        <button
+      <div className="coinbase w-[80%] h-[25%] flex flex-col items-center w-[80%] h-[25%]">
+        <div
           onClick={connectCoinbaseWallet}
-          className="flex p-10 items-center justify-center bg-white rounded-lg hover:scale-105"
+          className="flex  items-center w-[80%] h-[80%] justify-center bg-white rounded-lg hover:scale-105"
         >
-          <Image src="/wallet/Coinbase.svg" height={80} width={80}></Image>
-        </button>
+          <img src="/wallet/Coinbase.svg" className="self-center w-[50%]" />
+        </div>
         <p className="text-white text-center mt-2">Connect Coinbase</p>
       </div>
     </div>
